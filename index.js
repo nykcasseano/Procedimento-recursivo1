59d41265c5cb965f40f86f21def30bb303fc30df
nyk <nykcasseano13@gmail.com>
2022-08-15 15:54:12 -0300

    Versão final do código que corresponde ao exercício 2

---
+++ index.js
@@ -0,0 +1,14 @@
+function chunks(num) {
+    if (num === 0) {
+        return ''
+    }
+    if (num === 1) {
+        return 'chunk'
+    } else {
+        return 'chunk-' + chunks(num - 1)
+    }
+
+
+}
+
+console.log(chunks(5))
